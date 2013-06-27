5c34859abb43e4c0f535563901658ae5bda1c3c6
echociaran <ciaran@echo-peak.com>
2013-06-27 18:02:07 +0100

    Deleting redundant meta files
    Meta files belonging to deleted folders. Good that GitHub picks these
    up as they were a mess-generator in WOTD.

--- Code/Development/Main/Assets/Scripts/Audio/EPSound.cs
+++ Code/Development/Main/Assets/Scripts/Audio/EPSound.cs
@@ -19,6 +19,7 @@ public class EPSound : EPSoundEvent {
 	public float m_Pitch = 1.0f;
 	public float m_PitchVariance;
 	public PlayOrder m_PlayOrder;
+	//public EPMusicGlobals.MIDIpitch m_RootNote = EPMusicGlobals.MIDIpitch.C_4;
 	
 	public enum PlayOrder {
 		RANDOM,
@@ -29,17 +30,33 @@ public class EPSound : EPSoundEvent {
 	int m_Size;
 	int m_PlayIndex = -1;
 	
+	// Fade controls
+	bool m_Fading = false;
+	bool m_StopAfterFade = false;
+	float m_FadeStartLevel;
+	float m_FadeEndLevel;
+	float m_FadeStartTime;
+	float m_FadeDuration;
+	
 	// Use this for initialization
 	void Start ()
 	{
-		m_Size = m_Sources.GetLength(0);
+		GetSize();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-
+		if ( m_Fading )
+		{
+			ApplyFade();
+		}
 	}// Update
+	
+	void GetSize()
+	{
+		m_Size = m_Sources.GetLength(0);
+	}
 	
 	void m_UpdatePlayIndex()
 	{
@@ -60,12 +77,12 @@ public class EPSound : EPSoundEvent {
 		}
 		else
 		{
-			EchoDebugLog.LogWarning ("Invalid PlayOrder");
+			Debug.LogWarning ("Invalid PlayOrder");
 			m_PlayIndex = 0;
 		}
 	}
 	
-	
+	// Overrides for EPSoundEvent functions
 	public override void Play()
 	{
 		Play (1.0f, 1.0f);
@@ -83,7 +100,7 @@ public class EPSound : EPSoundEvent {
 		
 		// Play Sound
 		m_Sources[m_PlayIndex].Play();
-
+		
 		//Console.WriteLine("Playing sound " + (m_PlayIndex+1) + " of " + m_Size);
 	}
 	
@@ -109,17 +126,6 @@ public class EPSound : EPSoundEvent {
 		}
 	}
 	
-	public override void UpdateVolume(float newVolume)
-	{
-		foreach ( AudioSource source in m_Sources )
-		{
-			if ( source != null && source.isPlaying )
-			{
-				source.volume = newVolume;
-			}
-		}
-	}
-	
 	public override void Resume()
 	{
 		foreach ( AudioSource source in m_Sources )
@@ -129,6 +135,59 @@ public class EPSound : EPSoundEvent {
 				source.Play();
 			}
 		}
+	}
+	
+	public override void SetVolume( float volume )
+	{
+		foreach ( AudioSource source in m_Sources )
+		{
+			if ( source.isPlaying )
+			{
+				source.volume = volume;
+			}
+		}
+	}
+	
+	public override float GetVolume()
+	{
+		foreach ( AudioSource source in m_Sources )
+		{
+			if ( source != null && source.isPlaying )
+			{
+				return source.volume;
+			}
+		}
+		return 0.0f;
+	}
+	
+	public override void SetFade( float endVol, float duration, bool isFadeOut )
+	{
+		m_FadeStartTime = Time.time;
+		m_FadeDuration = duration;
+		m_FadeStartLevel = GetVolume();
+		m_FadeEndLevel = endVol;
+		m_StopAfterFade = isFadeOut;
+		m_Fading = true;
+	}
+	
+	public override void ApplyFade()
+	{
+			float fadeClock = Time.time - m_FadeStartTime;
+			if ( fadeClock < m_FadeDuration )
+			{
+				float vol = m_FadeStartLevel + (( fadeClock / m_FadeDuration ) * ( m_FadeEndLevel - m_FadeStartLevel ));
+				SetVolume ( vol );
+			}
+			else if ( !m_StopAfterFade )
+			{
+				SetVolume ( m_FadeEndLevel );
+				m_Fading = false;
+			}
+			else
+			{
+				Stop ();
+				m_Fading = false;
+			}
 	}
 	
 	public override bool IsPlaying()
