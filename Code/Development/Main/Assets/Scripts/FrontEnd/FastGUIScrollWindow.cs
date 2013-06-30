9697142cacbca0e259a1b582e44f326e60f1bf42
eddaly <edward.daly@gmail.com>
2013-06-30 11:13:28 +0100

    Finished touch based scrolling

--- Code/Development/Main/Assets/Scripts/FrontEnd/FastGUIScrollWindow.cs
+++ Code/Development/Main/Assets/Scripts/FrontEnd/FastGUIScrollWindow.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class FastGUIScrollWindow : FastGUIElement
 {
-	Vector2 firstPos, lastPos, velocity, movement;
-	bool decelerate = false;
+	Vector2 lastPos, velocity, movement;
+	bool autoscroll = false;
 	
 	public FastGUIScrollWindow (
 		Vector2 topLeft,	// Co-ords origin is top-left in pixels
@@ -19,72 +19,68 @@ public class FastGUIScrollWindow : FastGUIElement
 	{
 		//***if in area
 		
-		TextureAtlas frontendAtlas = PrimitiveLibrary.Get.m_Atlases[(int)TextureAtlas.AtlasID.FRONTEND];		
+		//TextureAtlas frontendAtlas = PrimitiveLibrary.Get.m_Atlases[(int)TextureAtlas.AtlasID.FRONTEND];		
 			
-		// Get movement since last Update
 		Vector2 touchPos;
+		
+		// If just touched the screen
 #if UNITY_IPHONE
-		if (Input.GetTouch(0).phase == TouchPhase.Began)
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
 #else
 		if (Input.GetMouseButtonDown (0))
 #endif
 		{
-			getTouchPos (out firstPos);
-			lastPos = firstPos;
+			autoscroll = false;			// Stop previous autoscroll
+			getTouchPos (out lastPos);	// Record initial position, used to set initial velocity of autoscroll
 		}
+		
+		// If moved the touch
 #if UNITY_IPHONE
-		if (Input.GetTouch(0).phase == TouchPhase.Moved)
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
 #else
 		if (Input.GetMouseButton (0))
 #endif
 		{
 			getTouchPos (out touchPos);
-			velocity = touchPos - firstPos;
-			movement = touchPos - lastPos;
-			lastPos = touchPos;
-		}
-#if UNITY_IPHONE
-		if (Input.GetTouch(0).phase == TouchPhase.Stationary)
-#else
-		if (movement.magnitude < Mathf.Epsilon)
+#if UNITY_IPHONE			
+			velocity = Input.GetTouch(0).deltaPosition / Input.GetTouch(0).deltaTime; // Velocity for autoscroll (touch only)
 #endif
-		{
-			// Stop still
-			velocity = Vector2.zero;
+			movement = touchPos - lastPos;	// Movement in frame
+			lastPos = touchPos;
+
+			// Move the quad position
+			Vector3 m3 = new Vector3 (movement.x, 0, 0); // X-axis only
+			m3 *= 3f;	// Scale to scroll with finger position (set with trial and error on iPhone5!)
+			if (m3.magnitude > 10) // Unless barely moving (at least with touch)
+				quad.m_Position += m3;
 		}
 		
+		// If touch ended start autoscroll
 #if UNITY_IPHONE
-		if (Input.GetTouch(0).phase == TouchPhase.Ended)
-#else
-		if (Input.GetMouseButtonUp (0))
-#endif
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
 		{
-			// start decelerating
-			decelerate = true;
+			autoscroll = true;
+			movement = Vector2.zero;
 		}
-		if (decelerate)
+		
+		// Autoscroll
+		if (autoscroll)
 		{
 			Vector3 v3 = new Vector3 (velocity.x, 0, 0);
-			v3 *= .1f;
+			v3 *= Time.deltaTime;	// Scale scroll speed to framerate
 			quad.m_Position += v3;
-			// Scroll the UVs
-			
 			//frontendAtlas.m_UVSet[textureIdx].x += v3.x / originalAtlasPixelsWidth;
 			//if (frontendAtlas.m_UVSet[textureIdx].x > frontendAtlas.m_UVSet[textureIdx].x //***clamp both sides
 																							//***set it up to be initial window
 																							//*** and will need max window to clamp to
-			velocity = velocity * .99f;
+			// Decelerate until stop
+			velocity = velocity * .9f;
 			if (velocity.magnitude  < Mathf.Epsilon)
 			{
-				decelerate = false;
+				autoscroll = false;
 			}
 		}
-		else
-		{
-			Vector3 m3 = new Vector3 (movement.x, 0, 0);
-			m3 *= 3f;
-			quad.m_Position += m3;
-		}
+#endif
 	}
 }
 
