fcc55f266157e7b739bf7d6c3d788d30f9988d2c
eddaly <edward.daly@gmail.com>
2013-07-02 22:08:32 +0100

    remove hack that was unhelpful for GUI

--- Code/Development/Main/Assets/Scripts/PrimitiveLibrary/Primitives/PrimitiveQuadBatch.cs
+++ Code/Development/Main/Assets/Scripts/PrimitiveLibrary/Primitives/PrimitiveQuadBatch.cs
@@ -250,7 +250,7 @@ public class PrimitiveQuadBatch
 			if( quadDef.m_Active )
 			{
 				//	Needed to avoid overflowing texels
-				const float tempHackFudge = 0.002f;
+				const float tempHackFudge = 0;//.002f;
 				
 				info.m_UVs[vert] = new Vector2
 					( atlas.m_UVSet[quadDef.m_TextureIdx].x + tempHackFudge, atlas.m_UVSet[quadDef.m_TextureIdx].y + tempHackFudge );
