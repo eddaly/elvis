7407bd4c6432e398a71b18a5f8609e34b40125c2
eddaly <edward.daly@gmail.com>
2013-07-02 14:00:54 +0100

    Broke out metagame data into own, easier to edit, script

---
+++ Code/Development/Main/Assets/Scripts/FrontEnd/Metagame.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using System.Collections;
+
+public class Metagame
+{
+	// Hard currency price (either Coins or cash)
+	public int m_GoldDiscsPriceCoins = 10000;
+	public float m_GoldDiscsPriceCashGBP = .99f;
+	
+	// Upgrades (need both Coins and GDs)
+	public struct Upgrade
+	{
+		public string name;
+		public int unlockLevel;
+		public int priceCoins;
+		public int priceGoldDiscs;
+	};
+	static public Upgrade [] upgrades = {
+		new Upgrade {name = "JumpsuitRed",		unlockLevel = 1,	priceCoins = 5000,		priceGoldDiscs = 1},
+		new Upgrade {name = "JumpsuitSilver",	unlockLevel = 10,	priceCoins = 25000,		priceGoldDiscs = 5},
+		new Upgrade {name = "JumpsuitGold", 	unlockLevel = 20,	priceCoins = 150000,	priceGoldDiscs = 20}
+	};
+	public enum UpgradesSave
+	{
+		JUMPSUIT_NONE = 0,
+		JUMPSUIT_RED,
+		JUMPSUIT_SILVER,
+		JUMPSUIT_GOLD
+	};	
+	
+	// Items (need both Coins and GDs)
+	public struct Item
+	{
+		public string name;
+		public int unlockLevel;
+		public int priceCoins;
+		public int priceGoldDiscs;
+	};
+	static public Item [] items = {
+		new Item {name = "BlueSuedeShoes",	unlockLevel = 5,	priceCoins = 10000,		priceGoldDiscs = 0},
+		new Item {name = "SpeedwayHelmet",	unlockLevel = 12,	priceCoins = 40000,		priceGoldDiscs = 1},
+		new Item {name = "ArmyGun", 		unlockLevel = 16,	priceCoins = 100000,	priceGoldDiscs = 5}
+	};
+	// Bitfield
+	public enum ItemsSave
+	{
+		BLUESUEDESHOES	= 1 << 0,
+		SPEEDWAYHELMET	= 1 << 1,
+		ARMYGUN			= 1 << 2
+	};
+
+	// Level-up cost (20pc increase rounded), note for convienience level[0] is meaningless and level[1] is free
+	static public int [] levelUpXP = {-1, 0, 
+		500, 600, 700, 900, 1000, 1200, 1500, 1800, 2100, 2600, 3100, 3700, 4500, 5300, 6400, 7700, 9200, 11100, 13300};
+	static public int maxLevels = levelUpXP.Length;
+	
+	// Missions
+	public struct Mission
+	{
+		public string name;
+		public int coinsPrize;
+		public int coinsPrice;
+		public int goldDiscsPrice;
+	};
+	static public Mission [] missions = {
+		new Mission {name = "Mission1", coinsPrize = 50, coinsPrice = 500, goldDiscsPrice = 1},
+		new Mission {name = "Mission2", coinsPrize = 100, coinsPrice = 1000, goldDiscsPrice = 2}
+	};
+}
+
--- Code/Development/Main/Assets/Scripts/FrontEnd/PersistentData.cs
+++ Code/Development/Main/Assets/Scripts/FrontEnd/PersistentData.cs
@@ -14,57 +14,6 @@ using System.Collections;
 
 public class PersistentData : MonoBehaviour {
 	
-	//*************************************************************** Editor parameters and defaults
-	
-	// Hard currency price (either Coins or cash)
-	public int m_GoldDiscsPriceCoins = 10000;
-	public float m_GoldDiscsPriceCashGBP = .99f;
-	
-	// Upgrades price (need both Coins and GDs)
-	public int m_JumpsuitRedUnlockLevel = 1;
-	public int m_JumpsuitRedPriceCoins = 5000;
-	public int m_JumpsuitRedPriceGoldDiscs = 1;
-	public int m_JumpsuitSilverUnlockLevel = 10;
-	public int m_JumpsuitSilverPriceCoins = 25000;
-	public int m_JumpsuitSilverPriceGoldDiscs = 5;
-	public int m_JumpsuitGoldUnlockLevel = 20;
-	public int m_JumpsuitGoldPriceCoins = 150000;
-	public int m_JumpsuitGoldPriceGoldDiscs = 20;
-
-	// Items price (need both XP and GDs)
-	public int m_BlueSuedeShoesUnlockLevel = 5;
-	public int m_BlueSuedeShoesPriceCoins = 10000;
-	public int m_BlueSuedeShoesPriceGoldDiscs = 0;
-	public int m_SpeedwayHelmetUnlockLevel = 12;
-	public int m_SpeedwayHelmetPriceCoins = 40000;
-	public int m_SpeedwayHelmetPriceGoldDiscs = 1;
-	public int m_ArmyGunUnlockLevel = 16;
-	public int m_ArmyGunPriceCoins = 100000;
-	public int m_ArmyGunPriceGoldDiscs = 5;
-	
-	// Level-up cost (20pc increase rounded)
-	public int m_LevelUpXP2 = 500;
-	public int m_LevelUpXP3 = 600;
-	public int m_LevelUpXP4 = 700;
-	public int m_LevelUpXP5 = 900;
-	public int m_LevelUpXP6 = 1000;
-	public int m_LevelUpXP7 = 1200;
-	public int m_LevelUpXP8 = 1500;
-	public int m_LevelUpXP9 = 1800;
-	public int m_LevelUpXP10 = 2100;
-	public int m_LevelUpXP11 = 2600;
-	public int m_LevelUpXP12 = 3100;
-	public int m_LevelUpXP13 = 3700;
-	public int m_LevelUpXP14 = 4500;
-	public int m_LevelUpXP15 = 5300;
-	public int m_LevelUpXP16 = 6400;
-	public int m_LevelUpXP17 = 7700;
-	public int m_LevelUpXP18 = 9200;
-	public int m_LevelUpXP19 = 11100;
-	public int m_LevelUpXP20 = 13300;
-	static int maxLevels = 20;
-	static private int [] levelUpXP;
-	
 	// This is a singleton
 	public static PersistentData instance {get; private set;}
 		
@@ -109,49 +58,55 @@ public class PersistentData : MonoBehaviour {
 	static public int CurrentLevel ()
 	{
 		int n;
-		for (n = 1; n < maxLevels; n++)
+		for (n = 1; n < Metagame.maxLevels; n++)
 		{
-			if (xP < levelUpXP[n + 1])
+			if (xP < Metagame.levelUpXP[n + 1])
 				break;		
 		}
 		return n;
 	}
 
 	//*************************************************************** Items
-	public enum Items
+	static public Metagame.ItemsSave itemsSave
 	{
-		BLUESUEDESHOES	= 1 << 0,
-		SPEEDWAYHELMET	= 1 << 1,
-		ARMYGUN			= 1 << 2
-	};
-	static public Items items
-	{
-		get {return (Items)SecuredPlayerPrefs.GetInt ("items", 0);}
+		get {return (Metagame.ItemsSave)SecuredPlayerPrefs.GetInt ("items", 0);}
 		set {
 			SecuredPlayerPrefs.SetInt ("items", (int)value);
 			Save ();	// Save here to ensure not lost if crash
 		}
 	}
-	static public bool HasItem (Items item) {
-		return ((int)items & (int)item) != 0;
+	static public bool HasItem (Metagame.ItemsSave item) {
+		return ((int)itemsSave & (int)item) != 0;
 	}
 	
 	//*************************************************************** Upgrades
-	public enum Upgrades
+	static public Metagame.UpgradesSave upgradesSave
 	{
-		JUMPSUIT_NONE = 0,
-		JUMPSUIT_RED,
-		JUMPSUIT_SILVER,
-		JUMPSUIT_GOLD
-	};	
-	static public Upgrades upgrades
-	{
-		get {return (Upgrades)SecuredPlayerPrefs.GetInt ("upgrades", 0);}
+		get {return (Metagame.UpgradesSave)SecuredPlayerPrefs.GetInt ("upgrades", 0);}
 		set {
 			SecuredPlayerPrefs.SetInt ("upgrades", (int)value);
 			Save ();	// Save here to ensure not lost if crash
 		}
 	}
+	
+	//*************************************************************** Missions
+	static int [] zeroArray = {};
+	static public int [] missionsComplete // 0 or 1, done or not done
+	{
+		get {return SecuredPlayerPrefs.GetIntArray ("missionsComplete", zeroArray);}
+		set {
+			SecuredPlayerPrefs.SetIntArray ("missionsComplete", value);
+			Save ();	// Save here to ensure not lost if crash
+		}
+	}
+	static public int [] missionsActive // index into Metagame.missions
+	{
+		get {return SecuredPlayerPrefs.GetIntArray ("missionsActive", zeroArray);}
+		set {
+			SecuredPlayerPrefs.SetIntArray ("missionsActive", value);
+			Save ();	// Save here to ensure not lost if crash
+		}
+	}	
 	
 	//*************************************************************** Public API
 	
@@ -172,29 +127,6 @@ public class PersistentData : MonoBehaviour {
 		// Initialise the encryption (don't change the key!)
 		SecuredPlayerPrefs.SetSecretKey("kqcgLEJFUxUSFkWhUrjC6");
 		//Debug.Log (Application. persistentDataPath); Note the savefile stored here
-			
-		// Set up the array equivalent
-		levelUpXP = new int [maxLevels+2];
-		levelUpXP[0] = levelUpXP[1] = 0;
-		levelUpXP[2] = m_LevelUpXP2;
-		levelUpXP[3] = m_LevelUpXP3;
-		levelUpXP[4] = m_LevelUpXP4;
-		levelUpXP[5] = m_LevelUpXP5;
-		levelUpXP[6] = m_LevelUpXP6;
-		levelUpXP[7] = m_LevelUpXP7;
-		levelUpXP[8] = m_LevelUpXP8;
-		levelUpXP[9] = m_LevelUpXP9;
-		levelUpXP[10] = m_LevelUpXP10;
-		levelUpXP[11] = m_LevelUpXP11;
-		levelUpXP[12] = m_LevelUpXP12;
-		levelUpXP[13] = m_LevelUpXP13;
-		levelUpXP[14] = m_LevelUpXP14;
-		levelUpXP[15] = m_LevelUpXP15;
-		levelUpXP[16] = m_LevelUpXP16;
-		levelUpXP[17] = m_LevelUpXP17;
-		levelUpXP[18] = m_LevelUpXP18;
-		levelUpXP[19] = m_LevelUpXP19;
-		levelUpXP[20] = m_LevelUpXP20;
 		
 		// Test
 		Debug.Log (SecuredPlayerPrefs.ToPrettyString());
