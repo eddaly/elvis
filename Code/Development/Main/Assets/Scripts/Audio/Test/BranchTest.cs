22b687b8cb8fc85c72ea022482bd55e1bf0ec8e0
echociaran <ciaran@echo-peak.com>
2013-07-31 11:34:16 +0100

    Modified test file in master

--- Code/Development/Main/Assets/Scripts/Audio/Test/BranchTest.cs
+++ Code/Development/Main/Assets/Scripts/Audio/Test/BranchTest.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class BranchTest : MonoBehaviour {
 	
 	// This is a new script file to commit to my test branch
+	// Modification in Master
+	
 	
 	// Use this for initialization
 	void Start ()
