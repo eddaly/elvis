9d93023a4d3d94ef38b8b63a06a89d2b1789f043
echociaran <ciaran@echo-peak.com>
2013-07-31 11:20:11 +0100

    Create a new file in my test branch

---
+++ Code/Development/Main/Assets/Scripts/Audio/Test/BranchTest.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class BranchTest : MonoBehaviour {
+	
+	// This is a new script file to commit to my test branch
+	
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+}
