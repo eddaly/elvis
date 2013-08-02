c81f2f0a2a3548f83a558ef0f49403fa5be579cd
echofraser <fraser@echo-peak.com>
2013-08-02 17:34:29 +0100

    Updated latest WIP - store.cs, atlases etc.

--- Code/Development/Main/Assets/Scripts/FrontEnd/store.cs
+++ Code/Development/Main/Assets/Scripts/FrontEnd/store.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 using System.Collections;
 
 public class store : MonoBehaviour {
+	
 #pragma warning disable 414
-//	FastGUIElement Store_Background;
+	FastGUIElement Store_Background;
 	/* FastGUIElement General_BackButton;
 	FastGUIButton BackButton;
 	FastGUIElement Store_Upgrade1Tab;
@@ -40,6 +41,7 @@ public class store : MonoBehaviour {
 	FastGUIElement Store_Upgrade1Tab;
 	FastGUIElement Store_Upgrade2Tab;
 	FastGUIElement Store_Upgrade3Tab;
+	FastGUIElement Store_GearEPIcon;
 #pragma warning restore 414
 
 	// Use this for initialization
@@ -48,6 +50,10 @@ public class store : MonoBehaviour {
 		
 		// The XML file containing the atlas UVs
 		FastGUIElement.uvxmlFile = @"assets//resources//Elvis_StoreAtlas.xml";
+		
+		Store_Background = new FastGUIElement (
+			new Vector2 (0, 0),
+			FastGUIElement.UVsFrom (@"Store_Background.png"));
 		
 		Store_WardrobeScreen = new FastGUIElement (
 			new Vector2 (192, 0),
@@ -95,6 +101,13 @@ public class store : MonoBehaviour {
 		
 		Store_Upgrade3Tab = new FastGUIElement (
 			new Vector2 (64, 896),
+			FastGUIElement.UVsFrom (@"Store_Upgrade3Tab.png"));
+		Store_Upgrade3Tab.SetDisplayed (false);
+		
+		//Gear Screen...
+		
+		Store_GearEPIcon = new FastGUIElement (
+			new Vector2 (64, 256),
 			FastGUIElement.UVsFrom (@"Store_Upgrade3Tab.png"));
 		Store_Upgrade3Tab.SetDisplayed (false);
 
