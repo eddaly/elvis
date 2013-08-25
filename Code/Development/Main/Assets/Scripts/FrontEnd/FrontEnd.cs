027304a6cd3e670309aff8133eaad2aa018a4b97
eddaly <edward.daly@gmail.com>
2013-08-25 21:17:19 +0100

    Added DisplayMessage() to enable modal messages to user

--- Code/Development/Main/Assets/Scripts/FrontEnd/FrontEnd.cs
+++ Code/Development/Main/Assets/Scripts/FrontEnd/FrontEnd.cs
@@ -5,10 +5,12 @@ public class FrontEnd : MonoBehaviour {
 	
 	[HideInInspector]
 	public PersistentData persistentData;
-	public Metagame metagame;
-	public string m_AtlasFile = "Frontend_Atlas";
-	public int m_AtlasOriginalWidth = 4096;
-	public int m_AtlasOriginalHeight = 4096;
+	private Metagame metagame;
+	public readonly string m_AtlasFile = "Frontend_Atlas";
+	public readonly int m_AtlasOriginalWidth = 4096;
+	public readonly int m_AtlasOriginalHeight = 4096;
+	private static string displayMessage = null;
+	private static GUISkin displayMessageGUISkin;
 	
 	// This is effectively a singleton
 	public static FrontEnd instance {get; private set;}
@@ -36,7 +38,6 @@ public class FrontEnd : MonoBehaviour {
 		}
 		else
 			persistentData = t.GetComponent<PersistentData>();
-		Debug.Log (persistentData);
 		
 		// Ensure camera as assumed by FastGUI
 		Camera.main.transform.position = new Vector3 (0, 0, -10);
@@ -46,6 +47,9 @@ public class FrontEnd : MonoBehaviour {
 		Camera.main.orthographicSize = 768;
 		Camera.main.nearClipPlane = .3f;
 		Camera.main.farClipPlane = 11;
+		
+		// Load the GUISkin used for messages
+		displayMessageGUISkin = Resources.Load ("FrontEnd_GUISkin") as GUISkin;
 	}
 	
 	// Update is called once per frame
@@ -53,4 +57,39 @@ public class FrontEnd : MonoBehaviour {
 	{
 		FastGUIElement.DebugDrawSafeArea ();
 	}
+	
+	// Ignore all inputs if true (required for ModalWindow as Unity still updating Monos, I guess just not GUIs)
+	public static bool IgnoreInputs ()
+	{
+		return displayMessage != null;
+	}
+	
+	// Display a modal message to the user, tap to dismiss, style as per Resource FrontEnd_GUISkin
+	public static void DisplayMessage (string message)
+	{
+		displayMessage = message;
+	}
+
+	// Used for DisplayMessage()
+	void OnGUI ()
+	{
+		if (displayMessage != null)
+		{
+			GUI.ModalWindow (0, new Rect (0, 0, Screen.width, Screen.height), 
+				ModalUserMessage, displayMessage, displayMessageGUISkin.GetStyle ("Box"));
+		}
+	}
+	
+	// Display the message until tapped to dismiss
+	private void ModalUserMessage (int windowID)
+	{
+		if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Ended)
+			displayMessage = null;
+		if (Application.isEditor)
+		{
+			if (Input.GetMouseButtonUp(0))
+				displayMessage = null;
+		}
+	}
+
 }
