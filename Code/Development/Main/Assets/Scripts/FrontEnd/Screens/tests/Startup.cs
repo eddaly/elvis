1d89e46b237c00238ffc18de378bf42d68290a57
eddaly <edward.daly@gmail.com>
2013-11-07 10:34:55 +0000

    WIP multiplayer code

--- Code/Development/Main/Assets/Scripts/FrontEnd/Screens/tests/Startup.cs
+++ Code/Development/Main/Assets/Scripts/FrontEnd/Screens/tests/Startup.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections;
 
+using UnityEngine.SocialPlatforms.GameCenter;
+
 public class Startup : MonoBehaviour {
 	
 	FastGUIElement playFGE, persistentDataFGE;
@@ -19,12 +21,63 @@ public class Startup : MonoBehaviour {
 		persistentDataFGE = new FastGUIElement (
 			new Vector2 (0, playFGE.height),	// Screen position
 			new Rect (0, 768, 2048, 768));	// Atlas position
+		
+		/*Social.localUser.Authenticate (success => {
+			if (success) {
+				Debug.Log ("Authentication successful");
+				string userInfo = "Username: " + Social.localUser.userName + 
+					"\nUser ID: " + Social.localUser.id + 
+					"\nIsUnderage: " + Social.localUser.underage;
+				Debug.Log (userInfo);
+			}
+			else
+				Debug.Log ("Authentication failed");
+		});*/
+		GCTurnBasedMatchHelper.Start ();
+	}
+	
+	void AchievementCallback (bool success)
+	{
+		Debug.Log ("Achievement Callback!! " + success);
+	}
+	
+	void OnGUI ()
+	{
+		if (GUI.Button (new Rect (0, 0, 200, 100), "SHOW GC"))
+		{
+			Social.ShowLeaderboardUI ();
+			Social.ShowAchievementsUI ();
+		}
+		else if (GUI.Button (new Rect (0, 200, 200, 100), "DO ACHIEVEMENT"))
+		{
+			Social.ReportProgress ("com.echopeak.elvis.achievement.test", 100, AchievementCallback);
+		}
+		else if (GUI.Button (new Rect (0, 400, 200, 100), "GET SCORE"))
+		{
+			Social.ReportScore (999, "com.echopeak.elvis.lb.test", success => {
+				Debug.Log(success ? "Reported score successfully" : "Failed to report score");
+			});
+		}
+
+		if (GUI.Button(new Rect (Screen.width / 2 + 50, 50, 75, 50), "Find Match"))
+		{
+			GCTurnBasedMatchHelper.FindMatchWithMaxPlayers (2);
+			//callbacks?
+		}
+		
+		if (GUI.Button(new Rect (Screen.width / 2 + 50, 150, 75, 50), "Send Turn"))
+		{
+			GCTurnBasedMatchHelper.SendTurn(345);
+			//now look out for callback
+		}
+
+		//*** Note if querying call every 10 frames or so as these calls slow
 	}
 		
 	// Update is called once per frame
-	void Update () {
-			
-		if (playFGE.Tapped ())
+	void Update () {		
+		
+		/*if (playFGE.Tapped ())
 		{
 			Debug.Log ("PLAY");
 			FrontEnd.UnloadAtlases ();
@@ -34,6 +87,8 @@ public class Startup : MonoBehaviour {
 		{
 			Debug.Log ("PERSISTENT DATA");
 			Application.LoadLevel ("Store");
-		}
+		}*/
 	}
 }
+
+
